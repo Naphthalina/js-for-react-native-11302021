2e1603f425801d8bc570c45ff7b5bae0f4d40c9c
Naphthalina <bnassefuah@st.ug.edu.gh>
2024-05-18 00:29:28 -0700

    Completed task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -6,3 +6,29 @@ function processArray(numbers) {
 module.exports = {
     processArray
 };
+
+function formatArrayStrings(strings, numbers) {
+    
+    if (strings.length !== numbers.length) {
+        throw new Error('Arrays must be of equal length');
+    }
+
+    for (let i = 0; i < strings.length; i++) {
+        const string = strings[i];
+        const number = numbers[i];
+
+        if (number % 2 === 0) {
+            strings[i] = string.toUpperCase();
+        }
+      
+        else {
+            strings[i] = string.toLowerCase();
+        }
+    }
+
+    return strings;
+}
+
+module.exports = {
+    formatArrayStrings
+};
